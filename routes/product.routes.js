7f25056f309423b5630a3bf2271f688c0763f284
Shanmier20 <santosmier16@gmail.com>
2025-10-31 19:52:21 +0800

    Update product.routes.js

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -3,13 +3,9 @@ const router = express.Router();
 const productController = require("../controllers/product.controller");
 
 router.post("/", productController.create);
-
 router.get("/", productController.findAll);
-
 router.get("/:id", productController.findOne);
-
 router.put("/:id", productController.update);
-
 router.delete("/:id", productController.delete);
 
 module.exports = router;
